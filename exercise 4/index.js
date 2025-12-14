b7f2d16cc5ece5ef8f5f4c6d2bec88cb6419b22b
engcamaaromar <engcamaaromar@gmail.com>
2025-12-14 08:57:00 +0300

    exercise 6

--- exercise 4/index.js
+++ exercise 4/index.js
@@ -1,24 +1,18 @@
 
 
-// function  add(a,b){
-//     console.log(a+b)
-// }
-// add(10,20);
-// add(30,30);
-// add(1,2)
-
-
-// const addd=function(b,c){
-//     return b+c;
-// }
-
-// console.log(addd(12,33));
-
-
-const add=(x,y)=>{
-    return x+y;
+function  add(a,b){
+    console.log(a+b)
 }
-console.log(add(5,15));
+add(10,20);
+add(30,30);
+add(1,2)
+
+
+const addd=function(b,c){
+    return b+c;
+}
+
+console.log(addd(12,33));
 
 
 
---
+++ exercise 6/index.js
@@ -0,0 +1,8 @@
+
+let colors=["blou","red","green","yellow","black"];
+console.log(colors)[0]
+console.log(colors)[1]
+console.log(colors)[2]
+console.log(colors)[3]
+console.log(colors)[4];
+
