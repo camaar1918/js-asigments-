a00ea02f1b3ec92f9de416fe89cbc43c37901571
engcamaaromar <engcamaaromar@gmail.com>
2025-12-14 14:46:28 +0300

    ex10

---
+++ exercise 10/index.js
@@ -0,0 +1,8 @@
+let library=[
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925,  },
+
+    { title: '1984', author: 'George Orwell', year: 1949,  },
+  
+];
+console.log(library[0].title);
+console.log(library[1].author);
