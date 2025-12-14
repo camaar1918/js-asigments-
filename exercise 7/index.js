83a011f47dbf585ffec6dfd4889f222b0bab479a
engcamaaromar <engcamaaromar@gmail.com>
2025-12-14 13:22:39 +0300

    exercise 7

---
+++ exercise 7/index.js
@@ -0,0 +1,16 @@
+
+let colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+
+colors.push('orange');
+
+console.log(colors);
+
+colors.pop();
+
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+colors.unshift('pink');
+console.log(colors);
