c316903d88552199ef557dc91a1f477254f40db0
engcamaaromar <engcamaaromar@gmail.com>
2025-12-15 10:59:23 +0300

    ex15

---
+++ exercise 15/index.js
@@ -0,0 +1,4 @@
+const students = [{ name: "abdisamad", score: 80 }, { name: "ahmed", score: 90 }, { name: "ali", score: 85 }];
+
+for (const student of students) {
+    console.log(student.name+" scored "+student.score);};
