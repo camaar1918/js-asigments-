d90d2dde1f55ef349ce054f3eb00f2e550128d0e
engcamaaromar <engcamaaromar@gmail.com>
2025-12-19 09:50:20 +0300

    ex17

---
+++ exercise 17/index.js
@@ -0,0 +1,9 @@
+let temperature=15;
+
+if(temperature<0){
+    console.log("very cold");   
+} else if(temperature<=15){
+    console.log("cold");
+} else if (temperature<=25){
+    console.log("warm");
+}
