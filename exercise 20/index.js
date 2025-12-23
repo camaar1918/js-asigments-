3b889760362807b0decc61f7888eaa693f56245e
Abdirizakjaran <100abdisamad101@gmail.com>
2025-12-23 11:16:17 +0300

    exercise 22

---
+++ exercise 20/index.js
@@ -0,0 +1,3 @@
+const frouits=["apple","banana","mango","orange"];
+const resulty= frouits.map((frouit)=> frouit.length);
+console.log(resulty);
---
+++ exercise 22/index.js
@@ -0,0 +1,9 @@
+
+
+let colors=["red","green","blue"];
+
+let abdisamad=[kalid,ahmed,samatar]=colors;
+
+console.log(kalid);
+
+console.log(samatar);
