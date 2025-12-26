af289a13a2bdac5f009df3b2c041df97f821db2d
camaar1918 <engcamaaromar@gmail.com>
2025-12-26 12:29:26 +0300

    jjj

---
+++ exercise 35/index.js
@@ -0,0 +1,29 @@
+
+
+function change(){
+
+    const image=document.querySelector('.img');
+    const URL=prompt("enter you url")
+    
+    let border=prompt("soo gali borderk radiuskaada")
+
+    image.style.borderRadius=(border+'px')
+
+    let widith=prompt("so gali widithka")
+
+    image.style.widith=(widith+'px')
+    let height=prompt("so gali widithka")
+
+    image.style.height=(height+'px')
+    image.setAttribute('src',URL);
+
+
+
+
+   
+
+    
+
+  
+
+}
